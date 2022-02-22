02215f874e4a42280370661f26445ae65c46ca96
Kellie Manasco <kellie.manasco@gmail.com>
2022-02-21 20:53:01 -0500

    cleans up code and adds more comments

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
+// global variables
 let playerWins = 0;
 let computerWins = 0;
-const options = ["rock", "paper", "scissors"];
 const selections = document.querySelectorAll(".selections");
 
+// adds event listeners to each possible player selection in the UI and returns the first class name assigned to it
 selections.forEach((selection) => {
   selection.addEventListener("click", (e) => {
     playRound(e.target.classList[0]);
   });
 });
 
+// grabs a random selection for the AI
 function computerPlay(options) {
   let aiRand = Math.floor(Math.random() * options.length);
   return options[aiRand];
 }
 
+// main play function
 function playRound(playerSelection) {
-  let jsText = document.getElementById("jsText");
-  computerSelection = computerPlay(options);
+  const options = ["rock", "paper", "scissors"];
+  let computerSelection = computerPlay(options);
   let playerIndex = options.indexOf(playerSelection);
   let aiIndex = options.indexOf(computerSelection);
   let tally = playerIndex - aiIndex;
+  let jsText = document.querySelector("#jsText");
 
+  // checks for the winner and displays the results in the UI
   if (playerIndex == aiIndex) {
     jsText.innerHTML = `You tied.<hr>Wins: ${playerWins}<br>Losses: ${computerWins}`;
   } else if (tally == 1 || tally == -2) {
@@ -30,18 +35,18 @@ function playRound(playerSelection) {
   }
 
   // sets all selection elements to reset their background color to default
-
   selections.forEach((selection) => {
     selection.style.backgroundColor = "#00a896";
   });
 
   document.querySelector(`.${playerSelection}`).style.backgroundColor =
     "#028090";
-  document.getElementById(`${computerSelection}AI`).style.backgroundColor =
+  document.querySelector(`#${computerSelection}AI`).style.backgroundColor =
     "#028090";
   winnerCheck(jsText);
 }
 
+// checks if either the player or AI has reached 5 total wins
 function winnerCheck(jsText) {
   if (playerWins == 5) {
     jsText.innerText = `You've won! Congratulations!`;
